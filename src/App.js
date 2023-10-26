81523e5ac6b7c659c81c3f49f2188a8b7b56db7b
charinu <charin.up@gmail.com>
2023-10-26 18:41:47 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./Navbar";
+import Users from "./Users";
+import UserCreate from "./UserCreate";
+import UserUpdate from "./UserUpdate";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path = "/" element={<Users />} />
+        <Route path = "create" element={<UserCreate />} />
+        <Route path = "update/:id" element={<UserUpdate />} />
+      </Routes>
+
+
+      {/* <Users /> */}
+    </div>
+  );
+}
+
+export default App;
